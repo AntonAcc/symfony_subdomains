c49777045ced7943da436e059303c59dd4feeeea
Anton Acc <antonxacc@gmail.com>
2023-10-05 20:05:19 +0300

    Prevent first run error

--- src/Kernel.php
+++ src/Kernel.php
@@ -6,6 +6,7 @@ use App\Entity\ModuleRoute;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Throwable;
 
 class Kernel extends BaseKernel
 {
@@ -13,14 +14,17 @@ class Kernel extends BaseKernel
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $em = $this->container->get('doctrine.orm.default_entity_manager');
-        $repository = $em->getRepository(ModuleRoute::class);
-        $moduleRouteList = $repository->findAll();
+        try {
+            $em = $this->container->get('doctrine.orm.default_entity_manager');
+            $repository = $em->getRepository(ModuleRoute::class);
+            $moduleRouteList = $repository->findAll();
 
-        $resource = '.';
-        /** @var ModuleRoute $moduleRoute */
-        foreach ($moduleRouteList as $moduleRoute) {
-            $routes->import($resource, $moduleRoute->getRouteType())->host($moduleRoute->getHost());
+            $resource = '.';
+            /** @var ModuleRoute $moduleRoute */
+            foreach ($moduleRouteList as $moduleRoute) {
+                $routes->import($resource, $moduleRoute->getRouteType())->host($moduleRoute->getHost());
+            }
+        } catch (Throwable $e) {
         }
     }
 }
