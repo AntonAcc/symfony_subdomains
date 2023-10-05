33b6ddb9266d41fbb1de01031338745051426568
Anton Acc <antonxacc@gmail.com>
2023-10-05 16:36:54 +0300

    Move routes configuration to Kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,21 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        $routeList = [
+            ['module_one', 'domain1.test.com'],
+            ['module_two', 'domain2.test.com'],
+        ];
+        $resource = '.';
+        foreach ($routeList as [$type, $host]) {
+            $routes->import($resource, $type)->host($host);
+        }
+    }
 }
